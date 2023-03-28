333394295b9811452792610b0434ee255e86b69f
Vladimir <noise123q@mail.ru>
2023-03-28 23:23:12 +0300

    feat: added burger menu

--- Shelter/main.js
+++ Shelter/main.js
@@ -1,1 +1,23 @@
-console.log(100, "max");
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+const body = document.querySelector('body');
+const overflow = document.querySelector('.overflow');
+
+burger.addEventListener('click', openBurger);
+document.addEventListener('click', closeBurger);
+
+function openBurger () {
+    burger.classList.toggle('open');
+    nav.classList.toggle('open');
+    body.classList.toggle('no-scroll');
+    overflow.classList.toggle('show');
+}
+
+function closeBurger (evt) {
+    if (!evt.target.closest('.nav') && !evt.target.closest('.burger')) {
+        burger.classList.remove('open');
+        nav.classList.remove('open');
+        body.classList.remove('no-scroll');
+        overflow.classList.remove('show');
+    }
+}
