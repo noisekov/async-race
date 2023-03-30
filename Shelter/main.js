1633bfe8b6532f61edd40b501ca728af4686578f
Vladimir <noise123q@mail.ru>
2023-03-30 22:52:39 +0300

    feat:added modal window

--- Shelter/main.js
+++ Shelter/main.js
@@ -14,10 +14,33 @@ function openBurger () {
 }
 
 function closeBurger (evt) {
-    if (!evt.target.closest('.nav') && !evt.target.closest('.burger')) {
+    if (!evt.target.closest('.nav')
+    && !evt.target.closest('.burger')
+    && !evt.target.closest('.swiper__content-block')
+    && !evt.target.closest('.modal')) {
         burger.classList.remove('open');
         nav.classList.remove('open');
         body.classList.remove('no-scroll');
         overflow.classList.remove('show');
+        modalWidnow.classList.remove('open');
     }
+}
+
+const petsCard = document.querySelectorAll('.swiper__content-block');
+const modalWidnow = document.querySelector('.modal-window');
+const modalClose = document.querySelector('.modal__btn-close');
+
+petsCard.forEach(card => {
+    card.addEventListener('click', showModal);
+})
+modalClose.addEventListener('click', closeModal);
+
+function showModal () {
+    modalWidnow.classList.toggle('open');
+    body.classList.toggle('no-scroll');
+}
+
+function closeModal () {
+    modalWidnow.classList.remove('open');
+    body.classList.remove('no-scroll');
 }
