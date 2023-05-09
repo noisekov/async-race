f25a9aa0e63a52a42fb087c72736acb7d5e314cf
Vladimir <noise123q@mail.ru>
2023-05-09 11:48:19 +0300

    feat: init minesweeper task

---
+++ minesweeper/src/index.js
@@ -0,0 +1,9 @@
+import './index.html'
+import './index.scss';
+import cod from "./images/cod.png"
+
+import {mult, sum } from "./modules/calc"
+console.log(mult(2,6))
+console.log(sum(22,40))
+
+const img = new Image()
