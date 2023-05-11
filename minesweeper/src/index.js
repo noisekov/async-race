26fc16a7190000a52a930f57b61a23f2a29cd25a
Vladimir <noise123q@mail.ru>
2023-05-11 23:29:19 +0300

    feat: add color number

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -12,15 +12,26 @@ const createElement = (tag, className, text = '', column, row) => {
 };
 
 class Minesweeper {
+
   constructor(field, mine) {
     this.row = field;
     this.column = field;
     this.mine = mine;
     this.allField = [];
     this.mineField = new Set();
-    this.startApp();
     this.gameOver = false;
     this.firstClick = true;
+    this.objColor = {
+      1 : 'Red',
+      2 : 'Blue',
+      3 : 'Green',
+      4 : 'Indigo',
+      5 : 'Brown',
+      6 : 'DeepPink',
+      7 : 'DarkSlateGray',
+      8 : 'MediumPurple',
+    }
+    this.startApp();
   }
 
   startApp() {
@@ -40,7 +51,7 @@ class Minesweeper {
       for (let j = 1; j <= this.column; j += 1) {
         count += 1;
         row.push(`${count}`);
-        minesweeper.append(createElement('button', `box ${count}`, `${count}`, `${j}`, `${i}`));
+        minesweeper.append(createElement('button', `box ${count}`, '', `${j}`, `${i}`));
       }
       this.allField.push(row);
     }
@@ -63,9 +74,9 @@ class Minesweeper {
     }
     [...this.mineField].forEach(mine => {
       if (mine > 0) {
-        minesweeper.children[mine -1].innerText = '*';
+        // minesweeper.children[mine -1].innerText = '*';
         minesweeper.children[mine -1].classList.add('boomb');
-      } 
+      }
     });
     console.log([...this.mineField], 'мины');
   }
@@ -74,6 +85,7 @@ class Minesweeper {
     if (!this.gameOver) {
       if (evt.target.closest('.box')) {
         evt.target.closest('.box').classList.add('current');
+        evt.target.closest('.box').style.color = `${this.objColor[+evt.target.closest('.box').innerText]}`;
         if (this.firstClick) {
           this.firstClick = false;
           const firstElem = +evt.target.closest('.box').classList[1];
@@ -90,24 +102,24 @@ class Minesweeper {
 
   countMineAround() {
     const minesweeper = document.querySelector('.minesweeper');
-    const mapAllElemAddPlus = [];
+    const mapElNearMine = [];
     Array.from(minesweeper.children).forEach((box) => {
       if (box.classList.contains('boomb')) {
-        mapAllElemAddPlus.push(`${+box.dataset.column + 1} ${+box.dataset.row + 1}`);
-        mapAllElemAddPlus.push(`${+box.dataset.column - 1} ${+box.dataset.row - 1}`);
-        mapAllElemAddPlus.push(`${+box.dataset.column + 1} ${+box.dataset.row - 1}`);
-        mapAllElemAddPlus.push(`${+box.dataset.column - 1} ${+box.dataset.row + 1}`);
-        mapAllElemAddPlus.push(`${+box.dataset.column} ${+box.dataset.row + 1}`);
-        mapAllElemAddPlus.push(`${+box.dataset.column} ${+box.dataset.row - 1}`);
-        mapAllElemAddPlus.push(`${+box.dataset.column + 1} ${+box.dataset.row}`);
-        mapAllElemAddPlus.push(`${+box.dataset.column - 1} ${+box.dataset.row}`);
-      } else {
-        box.innerText = "";
+        mapElNearMine.push(`${+box.dataset.column + 1} ${+box.dataset.row + 1}`);
+        mapElNearMine.push(`${+box.dataset.column - 1} ${+box.dataset.row - 1}`);
+        mapElNearMine.push(`${+box.dataset.column + 1} ${+box.dataset.row - 1}`);
+        mapElNearMine.push(`${+box.dataset.column - 1} ${+box.dataset.row + 1}`);
+        mapElNearMine.push(`${+box.dataset.column} ${+box.dataset.row + 1}`);
+        mapElNearMine.push(`${+box.dataset.column} ${+box.dataset.row - 1}`);
+        mapElNearMine.push(`${+box.dataset.column + 1} ${+box.dataset.row}`);
+        mapElNearMine.push(`${+box.dataset.column - 1} ${+box.dataset.row}`);
       }
     })
-    mapAllElemAddPlus.forEach(val => {
+    mapElNearMine.forEach(val => {
       Array.from(minesweeper.children).forEach((box) => {
-        if (+box.dataset.column === +val.split(' ')[0] && +box.dataset.row === +val.split(' ')[1] && !box.classList.contains('boomb')) {
+        if (+box.dataset.column === +val.split(' ')[0]
+        && +box.dataset.row === +val.split(' ')[1]
+        && !box.classList.contains('boomb')) {
           let textInnerBox = +box.innerText;
           textInnerBox += 1;
           box.innerText = textInnerBox;
@@ -118,15 +130,15 @@ class Minesweeper {
 
   markMine(evt) {
     evt.preventDefault();
-    if (evt.target.closest('.box') && !evt.target.closest('.box').classList.contains('current')) {
+    if (evt.target.closest('.box')
+    && !evt.target.closest('.box').classList.contains('current')) {
       evt.target.closest('.box').classList.toggle('is-here');
-      evt.target.closest('.box').textContent = '';
     }
   }
 
   finishGame() {
     const container = document.querySelector('.container');
-    container.append(createElement('div', 'modal', 'Вы проиграли!'));
+    container.append(createElement('div', 'modal', 'Game over. Try again!'));
   }
 }
 new Minesweeper(10, 10);
