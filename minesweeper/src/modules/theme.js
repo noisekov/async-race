9959c0bf6a4cd4145df8969a9d188596592bd953
noisekov <noise123q@mail.ru>
2023-05-15 14:34:50 +0300

    feat: add support light and dark theme

--- minesweeper/src/modules/theme.js
+++ minesweeper/src/modules/theme.js
@@ -7,10 +7,22 @@ const changeTheme = () => {
   const theme = document.querySelector('.theme');
   theme.append(createElement('select', 'theme__select'));
   const select = document.querySelector('.theme__select');
-  select.append(createElement('option', 'theme__option', 'White theme'));
-  select.append(createElement('option', 'theme__option', 'Black theme'));
-  const option = document.querySelectorAll('.theme__option');
-  option[0].value = 'white';
-  option[1].value = 'black';
+  select.append(createElement('option', 'theme__option--white', 'White theme'));
+  select.append(createElement('option', 'theme__option--black', 'Black theme'));
+  const optionWhite = document.querySelector('.theme__option--white');
+  const optionBlack = document.querySelector('.theme__option--black');
+  optionWhite.value = 'light';
+  optionBlack.value = 'dark';
 };
 changeTheme();
+
+const htmlEl = document.getElementsByTagName('html')[0];
+const select = document.querySelector('.theme__select');
+select.addEventListener("change", function(){
+  toggleTheme(select.value);
+});
+
+const toggleTheme = (theme = 'light') => {
+  htmlEl.dataset.theme = theme;
+}
+toggleTheme()
