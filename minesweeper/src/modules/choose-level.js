b78912719b87bdffb5a28188fcab2bcb620cefd9
noisekov <noise123q@mail.ru>
2023-05-19 11:20:03 +0300

    feat: create interface for change level without logic

--- minesweeper/src/modules/choose-level.js
+++ minesweeper/src/modules/choose-level.js
@@ -1,33 +1,53 @@
 /* eslint-disable */
-//
-// import { createElement } from '../modules/create-node.js';
+
+import { createElement } from '../modules/create-node.js';
 // import { Minesweeper } from '../modules/app.js';
 
-// const createLevelMinesweeper = () => {
-//   const body = document.querySelector('body');
-//   body.append(createElement('div', 'container'));
-//   const container = document.querySelector('.container');
-//   container.append(createElement('div', 'wrapper'));
-//   const wrapper = document.querySelector('.wrapper');
-//   wrapper.prepend(createElement('div', 'level'));
-//   const level = document.querySelector('.level');
-//   level.append(createElement('label', 'level__label', 'easy'));
-//   level.append(createElement('label', 'level__label', 'medium'));
-//   level.append(createElement('label', 'level__label', 'hard'));
-//   const levelLabel = document.querySelectorAll('.level__label');
-//   levelLabel.forEach(label => {
-//     label.append(createElement('input', 'level__radio'));
-//   })
-// };
-// createLevelMinesweeper();
-// const levelCheckbox = document.querySelectorAll('.level__radio');
-// levelCheckbox.forEach((input, i) => {
-//   input.setAttribute('type', 'radio');
-//   input.setAttribute('name', 'level');
-//   input.id = i + 1;
-//   input.addEventListener('change', changeLevelMinesweeper);
-// })
-// levelCheckbox[0].checked = true;
+const createLevelMinesweeper = () => {
+  const body = document.querySelector('body');
+  body.append(createElement('div', 'container'));
+  const container = document.querySelector('.container');
+  container.append(createElement('div', 'wrapper'));
+  const wrapper = document.querySelector('.wrapper');
+  wrapper.prepend(createElement('div', 'level'));
+  const level = document.querySelector('.level');
+  level.append(createElement('label', 'level__label', 'easy (10 x 10)'));
+  level.append(createElement('label', 'level__label', 'medium (15 x 15)'));
+  level.append(createElement('label', 'level__label', 'hard (25 x 25)'));
+  const levelLabel = document.querySelectorAll('.level__label');
+  levelLabel.forEach(label => {
+    label.append(createElement('input', 'level__radio'));
+  })
+  level.append(createElement('div', 'level__range-wrapper'));
+  const levelRangeWrap = document.querySelector('.level__range-wrapper');
+  levelRangeWrap.append(createElement('input', 'level__range'));
+  const levelRange = document.querySelector('.level__range');
+  levelRange.setAttribute('type', 'range');
+  levelRange.setAttribute('min', '0');
+  levelRange.setAttribute('max', '99');
+  levelRange.addEventListener('input', changeRange);
+
+  levelRangeWrap.append(createElement('span', 'level__range-value', `Set mine ${levelRange.value}`));
+  const levelRangeVal = document.querySelector('.level__range-value');
+
+  function changeRange (evt) {
+    levelRangeVal.innerText = `Set mine ${evt.target.value}`;
+  }
+
+  
+  level.append(createElement('button', 'level__save', 'Save settings'));
+  // const levelSave = document.querySelector('.level__save');
+};
+createLevelMinesweeper();
+
+const levelCheckbox = document.querySelectorAll('.level__radio');
+levelCheckbox.forEach((input, i) => {
+  input.setAttribute('type', 'radio');
+  input.setAttribute('name', 'level');
+  input.id = i + 1;
+  // input.addEventListener('change', changeLevelMinesweeper);
+})
+levelCheckbox[0].checked = true;
 // let minesweeperEl;
 // if (levelCheckbox[0].checked) {
 //   minesweeperEl = new Minesweeper(10, 10);
