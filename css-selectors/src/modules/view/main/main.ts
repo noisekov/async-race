e71c244e5d32de860789fb09f63323c10273ed03
Vladimir <noise123q@mail.ru>
2023-07-01 12:11:35 +0300

    feat: add scroll behavior for wrapper, add text for css block

--- css-selectors/src/modules/view/main/main.ts
+++ css-selectors/src/modules/view/main/main.ts
@@ -145,6 +145,17 @@ export default class Main implements IObserver {
     new Element(
       mainEditorBodyLine
     ).getNode().innerHTML = `1 <br> 2 <br> 3 <br> 4 <br> 5 <br> 6 <br> 7 <br> 8 <br> 9 <br> 10 <br> 11 <br> 12 <br> 13 <br> 14 <br> 15 <br> 16 <br> 17 <br> 18 <br> 19 <br> 20`;
+    const mainEditorBodyText = {
+      tagName: "div",
+      classNames: ["editor__body-text"],
+      textContent: "",
+      parentNode: editorBody.getNode(),
+    };
+    new Element(mainEditorBodyText).getNode().innerHTML = `
+      {<br>
+      /* Styles would go here. */<br>
+      }
+    `;
     this.aside = new Aside();
     this.chooseLevel();
     return createNode;
