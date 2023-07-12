ef8be20266582492c7e431261c3638d9df2b3daa
Vladimir <noise123q@mail.ru>
2023-07-13 01:51:36 +0300

    feat: add input block

--- async-race/src/modules/app.ts
+++ async-race/src/modules/app.ts
@@ -1,7 +1,9 @@
 import Header from "./view/header/header.html";
+import Main from "./view/garage/garage.html";
 
 export default class App {
   public start(): void {
     document.body.insertAdjacentHTML("afterbegin", Header);
+    document.body.insertAdjacentHTML("beforeend", Main);
   }
 }
